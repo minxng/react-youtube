d0e33dfac7dd93c11d1944a07e024bc69ee40308
minxng <minxng12@gmail.com>
2024-09-25 20:19:06 +0900

    [style] search header 스타일 수정

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -12,20 +12,20 @@ export default function SearchHeader() {
     navigate(`/videos/${text}`);
   };
   return (
-    <header className="w-full p-4 text-2xl border-b border-zinc-600 mb-4 flex">
+    <header className="w-full p-4 border-b border-zinc-600 mb-4 flex">
       <Link to="/" className="flex items-center">
         <BsYoutube className="text-4xl text-brand" />
         <h1 className="font-bold ml-2 text-3xl">Youtube</h1>
       </Link>
       <form className="w-full flex justify-center " onSubmit={handleOnSubmit}>
         <input
-          className="w-7/12 p-2 outline-none text-zinc-900"
+          className="w-7/12 px-4 py-2 outline-none border rounded-l-full border-zinc-400 text-zinc-900 "
           type="text"
           placeholder="Search..."
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button className="px-4 ">
+        <button className="px-4 border rounded-r-full border-zinc-400 border-l-0">
           <BsSearch />
         </button>
       </form>
